952ad320a142fd9b1e812c1ef41a505d8cbb4bb3
Viktor Kulia <vitya.kulya@gmail.com>
2022-06-26 20:20:23 +0300

    Created Models for Networking

---
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Виктор Куля on 26.06.2022.
+//
+
+import Foundation
+
+struct Folloer {
+    var login: String
+    var avatarUrl: String
+}
